df8a868b476a884953f9900d252248e9a9bf52a9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-16 08:59:08 +0700

    divided creations into functions

--- main.js
+++ main.js
@@ -4,42 +4,57 @@ import * as three from "three";
 
 const scene = new three.Scene();
 
-const fieldOfView = 70;
-const aspect = window.innerWidth / innerHeight;
-const range = { near: 0.1, far: 1000 };
+const createCamera = function () {
+    const fieldOfView = 70;
+    const aspect = window.innerWidth / innerHeight;
+    const range = { near: 0.1, far: 1000 };
 
-const camera = new three.PerspectiveCamera(
-    fieldOfView,
-    aspect,
-    range.near,
-    range.far
-);
+    const camera = new three.PerspectiveCamera(
+        fieldOfView,
+        aspect,
+        range.near,
+        range.far
+    );
 
-const renderer = new three.WebGLRenderer({
-    antialias: true,
-});
+    camera.position.z = 5;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-
-document.body.appendChild(renderer.domElement);
-
-camera.position.z = 5;
-
-const cube = new three.Mesh(
-    new three.BoxGeometry(1, 1, 1),
-    new three.MeshBasicMaterial({ color: 0x00ff00 })
-);
-
-scene.add(cube);
-
-const animate = function () {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
+    return camera;
 };
 
-animate();
+const createRenderer = function () {
+    const renderer = new three.WebGLRenderer({
+        antialias: true,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    return renderer;
+};
+
+const main = function () {
+    const renderer = createRenderer();
+    const camera = createCamera();
+
+    document.body.appendChild(renderer.domElement);
+
+    const cube = new three.Mesh(
+        new three.BoxGeometry(1, 1, 1),
+        new three.MeshBasicMaterial({ color: new three.Color("rgb(255, 255, 255)") })
+    );
+
+    scene.add(cube);
+
+    const animate = function () {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    };
+
+    animate();
+};
+
+main();
